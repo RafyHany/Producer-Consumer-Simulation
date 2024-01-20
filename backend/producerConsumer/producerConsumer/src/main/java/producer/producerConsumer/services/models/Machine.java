64f91702a927151987c46594e4f617e31e3bceb0
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2024-01-20 17:25:02 +0200

    Updateed Machine.java

--- backend/producerConsumer/producerConsumer/src/main/java/producer/producerConsumer/services/models/Machine.java
+++ backend/producerConsumer/producerConsumer/src/main/java/producer/producerConsumer/services/models/Machine.java
@@ -1,13 +1,13 @@
 package producer.producerConsumer.services.models;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class Machine extends Element {
+public class Machine extends Element implements Runnable{
     private int operatingTime;
-    private List<Queues> inQueues = new ArrayList<>() ;
-    private Queues outQueue ;
-    private Product product ;
+    private List<Queues> inQueues = new ArrayList<>();
+    private Queues outQueue;
+    private Product product;
+
     public Machine(){}
     public Machine(ElementDto dto, int operatingTime) {
         super(dto);
@@ -41,11 +41,42 @@ public class Machine extends Element {
         return product;
     }
 
-    public void setProduct(Product product) {
-        this.product = product;
-    }
+
     public void addToInQueues(Queues queue){
         inQueues.add(queue);
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(operatingTime * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        outQueue.addToProducts(product);
+        product = null;
+        machineNotifyFree();
+    }
+
+        public void machineNotifyFree(){
+        this.setColor("#ddd");
+        for(Queues q: inQueues) {
+            q.makeMachineFree(this.getId());
+        }
+    }
+
+    public synchronized void setProduct(Product product) {
+        this.product = product;
+        machineNotifyBusy();
+        Thread thread = new Thread(this::run);
+        thread.start();
+        this.setColor(product.getColor());
+    }
+
+    public void machineNotifyBusy(){
+        for(Queues q: inQueues) {
+            q.makeMachineBusy(this.getId());
+        }
     }
 
 }
