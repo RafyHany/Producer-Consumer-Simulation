15da555d5458317b7b7435259c83361a996ef4d1
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2024-01-20 18:38:21 +0200

    Create CareTaker.java

---
+++ backend/producerConsumer/producerConsumer/src/main/java/producer/producerConsumer/services/simulation/CareTaker.java
@@ -0,0 +1,13 @@
+package producer.producerConsumer.services.simulation;
+
+class Caretaker {
+    private GraphMemento memento;
+
+    public GraphMemento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(GraphMemento memento) {
+        this.memento = memento;
+    }
+}
